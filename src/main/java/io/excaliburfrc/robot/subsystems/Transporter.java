35e319bcab81e2b49ed8c7c0832647fdebdf3488
hello921 <s1951967@ybalm.co.il>
2021-01-17 19:35:04 +0200

    add Transporter switch enums and buttons configer

--- src/main/java/io/excaliburfrc/robot/subsystems/Transporter.java
+++ src/main/java/io/excaliburfrc/robot/subsystems/Transporter.java
@@ -14,11 +14,23 @@ public class Transporter extends SubsystemBase {
     loading = new VictorSPX(LOADING_ID);
   }
 
-  public void setFlickerspeed(double speed) {
-    flicker.set(ControlMode.PercentOutput, speed);
+  public enum Mode {
+    IN(0.6),
+    OUT(-0.4),
+    OFF(0);
+
+    Mode(double i) {
+      speed = i;
+    }
+
+    public final double speed;
   }
 
-  public void setLoading(double speed) {
-    loading.set(ControlMode.PercentOutput, speed);
+  public void setFlicker(Mode speed) {
+    flicker.set(ControlMode.PercentOutput, speed.speed);
+  }
+
+  public void setLoading(Mode speed) {
+    loading.set(ControlMode.PercentOutput, speed.speed);
   }
 }
