037b7c24ae76f601b7fd8998bf6b20ee039f399b
Starlight220 <yotamshlomi@gmail.com>
2021-02-09 09:32:51 +0200

    Revert "add sim"
    This reverts commit 8b84bccc8fe0541d48af844c66d62a434c36a7d8.

--- src/main/java/io/excaliburfrc/lib/SimSparkMax.java
+++ src/main/java/io/excaliburfrc/lib/SimSparkMax.java
@@ -1,15 +1,12 @@
 package io.excaliburfrc.lib;
 
-import com.revrobotics.*;
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.hal.SimDouble;
-import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.simulation.SimDeviceSim;
 
 public class SimSparkMax extends CANSparkMax {
   private final SimDouble simappliedOutput;
-  private SimDeviceSim simDevice;
 
   /**
    * Create a new SPARK MAX Controller
@@ -22,8 +19,8 @@ public class SimSparkMax extends CANSparkMax {
   public SimSparkMax(int deviceID, MotorType type) {
     super(deviceID, type);
     if (RobotBase.isSimulation()) {
-      simDevice = new SimDeviceSim("SPARK MAX [" + deviceID + "]");
-      simappliedOutput = simDevice.getDouble(CanUtil.ctrlToKey(ControlType.kDutyCycle));
+      simappliedOutput =
+          new SimDeviceSim("SPARK MAX [" + deviceID + "]").getDouble("Applied Output");
     } else simappliedOutput = null;
   }
 
@@ -33,45 +30,5 @@ public class SimSparkMax extends CANSparkMax {
       simappliedOutput.set(speed);
     }
     super.set(speed);
-  }
-
-  @Override
-  public CANPIDController getPIDController() {
-    return new CANPIDControllerSim(this, simDevice);
-  }
-
-  private static final class OnboardControl {
-    private final PIDController controller;
-    private final SimDouble input, output;
-
-    public OnboardControl(CANPIDController controller, SimDouble input, SimDouble output) {
-      this.controller = new PIDController(controller.getP(), controller.getI(), controller.getD());
-      this.input = input;
-      this.output = output;
-    }
-
-    private static final Notifier notifier = new Notifier(OnboardControl::run);
-    private static boolean isRunning = false;
-    private static final double PERIOD = 0.02;
-
-    static synchronized void start() {
-      if (isRunning) {
-        return;
-      }
-      notifier.startPeriodic(PERIOD);
-      isRunning = true;
-    }
-
-    static synchronized void stop() {
-      if (!isRunning) {
-        return;
-      }
-      notifier.stop();
-      isRunning = false;
-    }
-
-    private static void run() {}
-
-    private void calc() {}
   }
 }
