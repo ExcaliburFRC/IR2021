fa1059a506d5cf901ef3466af547ac4e3268938b
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2021-03-07 21:35:39 +0200

    add interpolation

--- src/main/java/io/excaliburfrc/robot/commands/galsearch/GalacticSearch.java
+++ src/main/java/io/excaliburfrc/robot/commands/galsearch/GalacticSearch.java
@@ -1,3 +1,30 @@
 package io.excaliburfrc.robot.commands.galsearch;
 
-public class GalacticSearch {}
+import edu.wpi.first.wpilibj.util.Units;
+
+public class GalacticSearch {
+  private enum Path {
+    AR,
+    AB,
+    BR,
+    BB;
+  }
+
+  private Path determinePath(double distance, double xOffset) {
+    if (distance < Units.inchesToMeters(100)) {
+      // we're doing red!
+      if (xOffset > 20) {
+        return Path.BR;
+      } else {
+        return Path.AR;
+      }
+    } else {
+      // we're doing blue!
+      if (xOffset > 16) {
+        return Path.AB;
+      } else {
+        return Path.BB;
+      }
+    }
+  }
+}
--- src/main/java/io/excaliburfrc/robot/subsystems/Vision.java
+++ src/main/java/io/excaliburfrc/robot/subsystems/Vision.java
@@ -1,33 +1,41 @@
 package io.excaliburfrc.robot.subsystems;
 
-
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import io.excaliburfrc.lib.vision.Limelight;
-
-import static io.excaliburfrc.robot.Constants.LIMELIGHT_FWD;
-import static io.excaliburfrc.robot.Constants.LIMELIGHT_REV;
+import io.excaliburfrc.lib.vision.Limelight.DataKey;
+import io.excaliburfrc.lib.vision.Limelight.SettingsKey;
+import io.excaliburfrc.robot.Constants;
 
 public class Vision extends SubsystemBase {
-    private Limelight limelight;
-    private DoubleSolenoid piston;
+  private final Limelight limelight;
+  private final DoubleSolenoid piston;
 
-    public Vision() {
-        limelight = new Limelight();
-        piston = new DoubleSolenoid(LIMELIGHT_FWD, LIMELIGHT_REV);
-    }
+  private static final int POWER_PORT_PIPELINE = 1;
+  private static final int POWER_CELL_PIPELINE = 2;
 
-    public void raise() {
-        piston.set(DoubleSolenoid.Value.kReverse);
-    }
+  public Vision() {
+    limelight = new Limelight();
+    piston = new DoubleSolenoid(Constants.LIMELIGHT_FWD, Constants.LIMELIGHT_REV);
+  }
 
-    public void lower() {
-        piston.set(DoubleSolenoid.Value.kForward);
-    }
+  public void raise() {
+    piston.set(DoubleSolenoid.Value.kReverse);
+    limelight.setData(SettingsKey.ActivePipeline, POWER_PORT_PIPELINE);
+  }
 
-    public double getDistance() {
-        //  TODO : calebrate
-        return -1;
-    }
-//    HasValidTargets("tv"),
+  public void lower() {
+    piston.set(DoubleSolenoid.Value.kForward);
+    limelight.setData(SettingsKey.ActivePipeline, POWER_CELL_PIPELINE);
+  }
+
+  public double getDistance() {
+    //  TODO : calibrate [regression]
+    return -1;
+  }
+
+  public double getXOffset() {
+    return limelight.getData(DataKey.XOffset);
+  }
+
 }
