a614643963c1665bbb75ea2a147117a5808a8c37
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2021-03-14 00:09:01 +0200

    spotless+ratchet

--- src/main/java/io/excaliburfrc/robot/subsystems/LEDs.kt
+++ src/main/java/io/excaliburfrc/robot/subsystems/LEDs.kt
@@ -5,15 +5,18 @@ import io.excaliburfrc.robot.Constants.LED_PORT
 import kotlin.properties.Delegates
 
 object LEDs {
-    private val leds = PWM(LED_PORT)
+  private val leds = PWM(LED_PORT)
 
-    enum class LedMode(val value: Double) {
-        BLUE(0.87), RED(0.61), GREEN(0.73), YELLOW(0.67), RAINBOW(-0.97), OFF(0.99)
+  enum class LedMode(val value: Double) {
+    BLUE(0.87),
+    RED(0.61),
+    GREEN(0.73),
+    YELLOW(0.67),
+    RAINBOW(-0.97),
+    OFF(0.99)
+  }
 
-
-    }
-
-    var mode: LedMode by Delegates.observable(LedMode.BLUE){ _, _, newValue ->
-            leds.speed = newValue.value
-        }
+  var mode: LedMode by Delegates.observable(LedMode.BLUE) { _, _, newValue ->
+    leds.speed = newValue.value
+  }
 }
